d25e8ceecbeaad9768e1de4d805a55f7e9c0817b
Dam Nhat Thong <nhatthong34@gmail.com>
2019-10-31 22:31:40 +0700

    update cors

--- server.js
+++ server.js
@@ -24,15 +24,21 @@ mongoose
 
 // Initializes application
 const app = express();
-
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000']
 // Enable cors
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 };
 
 app.use(cors());
